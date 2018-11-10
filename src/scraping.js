2e3212bd36ee95bd231e9aaa5ca4ce31d8bbcc95
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 23:07:56 +0900

    test

--- src/scraping.js
+++ src/scraping.js
@@ -6,11 +6,3 @@
 require('date-utils');
 
 
-
-var api_impl = {
-    "fs" : require('fs'),
-    "cheerioHttp" : require('cheerio-httpcli'),
-    "cheerio" : require('cheerio')
-};
-exports.api_impl = api_impl;
-
