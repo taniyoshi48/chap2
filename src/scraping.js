8faf6b502e5104afcb3fae63e42a788103a1134f
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 23:46:30 +0900

    test

--- src/scraping.js
+++ src/scraping.js
@@ -13,3 +13,9 @@ var api_impl = {
 
 };
 exports.api_impl = api_impl;
+
+var CACHE_PATH = "./data/cache.html";
+
+var EVENTE_TITLE = process.env.SEARCH_KEYWORD || "#3good";
+var SEARCH_KEYWORD = EVENTE_TITLE;
+SEARCH_KEYWORD = SEARCH_KEYWORD.replace(/#/, '%23');
