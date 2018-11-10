abb0d29bf007d18f2a31d272e67ac49986a7edf5
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 21:40:41 +0900

    tes

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-var http = require("http");
+var http = require('http');
 
-var router = require("./src/router_static.js");
-var api = require("./src/api.js")
+var server = http.createServer(function(request, response) {
 
-var port = process.env.PORT || 8037;
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.end("Hello World!");
 
-http.createServer(function (request, response) {
-    var promise = router.isStaticHtmlFound(  request, response  );
+});
 
-    promise.catch(function(){
-        // 静的htmlファイル予備では【なかった】場合に、RESTful API呼び出しとして扱う。
-        return api.responseApi( request, response );
-    });
-}).listen( port );
-console.log("Server has started. - http://127.0.0.1:" + port +"/");
+var port = process.env.PORT || 1337;
+server.listen(port);
+
+console.log("Server running at http://localhost:%d", port);
