631eb669a83ce6e40a6c7b3c9c4c68d78cf0f375
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 22:26:29 +0900

    test

--- src/api.js
+++ src/api.js
@@ -5,19 +5,3 @@
 var url = require("url")
 
 var authentication = require("./authentication.js");
-var scraping = require("./scraping.js");
-
-var HEADER_JSON = {
-    "Access-Control-Allow-Origin" : "*", // JSONはクロスドメインがデフォルトNG。
-    "Pragma" : "no-cacha", 
-    "Cache-Control" : "no-cache",
-    "Content-Type" : "application/json; charset=utf-8"
-};
-var HEADER_TEXT = {
-    "Access-Control-Allow-Origin" : "*",
-    "Pragma" : "no-cacha", 
-    "Cache-Control" : "no-cache",
-    "Content-Type" : "text/html"
-};
-
-
