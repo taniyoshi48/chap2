bf2d9e8b4f533de0bdd14385b9d3dfc4bd15be98
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 22:37:28 +0900

    test

--- src/scraping.js
+++ src/scraping.js
@@ -7,4 +7,17 @@ require('date-utils');
 
 
 
+var api_impl = {
+    "fs" : require('fs'),
+    "cheerioHttp" : require('cheerio-httpcli'),
+    "cheerio" : require('cheerio')
+};
+exports.api_impl = api_impl;
+
+var CACHE_PATH = "./data/cache.html";
+var EVENTE_TITLE = process.env.SEARCH_KEYWORD || "#3good";
+var SEARCH_KEYWORD = EVENTE_TITLE;
+SEARCH_KEYWORD = SEARCH_KEYWORD.replace(/#/, '%23');
+
+
 
