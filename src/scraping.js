757df5b450b6d9c091b95ecfe332791f7560f495
taniyoshi48 <taniyoshi48@yahoo.co.jp>
2018-11-10 23:00:55 +0900

    test

--- src/scraping.js
+++ src/scraping.js
@@ -14,7 +14,7 @@ var api_impl = {
 };
 exports.api_impl = api_impl;
 
-var CACHE_PATH = "./data/cachee.html";
+var CACHE_PATH = "";
 
 var EVENTE_TITLE = process.env.SEARCH_KEYWORD || "#3good";
 var SEARCH_KEYWORD = EVENTE_TITLE;
